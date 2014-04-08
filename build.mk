fb0334b788eb273c94ccc3934b839c6c61b270e9
calle <calle@cloudius-systems.com>
2014-04-08 14:43:17 +0200

    meh!

--- build.mk
+++ build.mk
@@ -104,7 +104,6 @@ CFLAGS = -std=gnu99 $(COMMON)
 CFLAGS += -I $(src)/libc/internal -I  $(src)/libc/arch/$(arch) \
 	-Wno-missing-braces -Wno-parentheses -Wno-unused-but-set-variable
 
-
 ASFLAGS = -g $(autodepend) -DASSEMBLY
 
 fs/vfs/main.o: CXXFLAGS += -Wno-sign-compare -Wno-write-strings
